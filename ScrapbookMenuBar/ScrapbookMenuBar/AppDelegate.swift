e4707288737c3ff4ee608089a09b4524e1cd60b8
hudh0827@vt.edu <36307709+DonghanHu@users.noreply.github.com>
2021-08-30 14:55:41 -0400

    created the project

---
+++ ScrapbookMenuBar/ScrapbookMenuBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScrapbookMenuBar
+//
+//  Created by Donghan Hu on 8/30/21.
+//  Copyright © 2021 Donghan Hu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScrapbookMenuBar/ScrapbookMenuBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScrapbookMenuBar
+//
+//  Created by Donghan Hu on 8/30/21.
+//  Copyright © 2021 Donghan Hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
